246d86c8982182bd32fcbea70d4743aa6c4363ff
Pratibha Ahlawat <pratibhaahlawat@gmail.com>
2024-10-19 22:44:53 +0530

    task 4

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    alert("Thank you for your message!");
+    // You can add more functionality here, like sending the form data to a server
+});
